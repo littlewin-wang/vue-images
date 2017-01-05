88a781fdb8f10bbbe5a04d53b5586fcb41ca5efb
littlewin <littlewin.wang@gmail.com>
2017-01-05 15:21:08 +0800

    更新thumbnail组件

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <icon :type="type" :color="color"></icon>
+  <div class="app">
+    <icon :type="type" :color="color" class="icon-x"></icon>
+    <thumbnail :imageUrl="imageUrl"></thumbnail>
+    <thumbnail :imageUrl="imageUrl"></thumbnail>
+    <thumbnail :imageUrl="imageUrl"></thumbnail>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
 import icon from './components/icon'
+import thumbnail from './components/thumbnail'
 
 export default {
   name: 'app',
   data () {
     return {
-      type: 'close',
-      color: 'yellow'
+      type: 'arrowRight',
+      color: '#22aa11',
+      imageUrl: 'https://images.unsplash.com/photo-1460899960812-f6ee1ecaf117?dpr=2&auto=format&crop=faces&fit=crop&w=240&h=159'
     }
   },
   components: {
-    icon
+    icon,
+    thumbnail
   }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .app
+    font-size: 0
+    background: #000
+    .icon-x
+      width: 20px
 </style>
---
+++ src/components/thumbnail.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="thumbnail" :style="{ backgroundImage: 'url(' + imageUrl + ')' }"></div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      imageUrl: String
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .thumbnail
+    display: inline-block
+    margin: 2px
+    width: 50px
+    height: 50px
+    border-radius: 2px
+    background-position: center
+    background-size: cover
+    box-shadow: inset 0 0 0 1px hsla(0,0%,100%,.2)
+    cursor: pointer
+    overflow: hidden
+    &.active
+      box-shadow: inset 0 0 0 2px #fff
+</style>
